c28ce9e3520f80573d6a916de94418f803544417
Christopher Chavez <chrischavez@gmx.us>
2020-11-29 10:06:31 -0600

    Use i2c_smbus_read_block_data() from kernel
    Available since 2.6.22
    See torvalds/linux@b86a1bc8e396

--- cfa779.c
+++ cfa779.c
@@ -62,22 +62,6 @@ I2C_CLIENT_INSMOD_1 (cfa779);
 
 /* ---------------------------------------------------------------------*/
 
-s32
-i2c_smbus_read_block_data (struct i2c_client *client, u8 command, u8 * values)
-{
-    union i2c_smbus_data data;
-    int i;
-    if (i2c_smbus_xfer (client->adapter, client->addr, client->flags,
-                        I2C_SMBUS_READ, command, I2C_SMBUS_BLOCK_DATA, &data))
-        return -1;
-    else
-      {
-          for (i = 1; i <= data.block[0]; i++)
-              values[i - 1] = data.block[i];
-          return data.block[0];
-      }
-}
-
 /* Each client has this additional data */
 struct cfa779_data
 {
